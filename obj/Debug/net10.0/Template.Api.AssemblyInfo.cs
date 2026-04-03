be0a2ee48a4c9f13ee1dd97cff4c82ed48393491
chasertx <chaseashby199@gmail.com>
2026-04-03 17:15:18 -0500

    adding folders for scaffolding

--- obj/Debug/net10.0/Template.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Template.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Template.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a92b709278e43d62ab54756db569ef4139ee64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Template.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Template.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
