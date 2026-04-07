a6c31be089d35dbce442ff0e64778683587ac5e1
Aaron Bading i9 <aaronbading.ab@gmail.com>
2026-04-07 12:45:03 -0500

    add verify password and generate token methods

--- Brokers/Security/ISecurityBroker.cs
+++ Brokers/Security/ISecurityBroker.cs
@@ -3,4 +3,6 @@ namespace Template.Api.Brokers.Security;
 public interface ISecurityBroker
 {
     string HashPassword(string password);
+    bool VerifyPassword(string password, string hashedPassword);
+    string GenerateToken(string userId, string email, string role);
 }
