a77bcb7a3fd046efb3ce28da9009949ff01d3298
chasertx <chaseashby199@gmail.com>
2026-04-10 13:14:52 -0500

    removing redundant securitybroker

--- Brokers/Security/IBroker.Security.cs
+++
@@ -1,8 +0,0 @@
-namespace Template.Api.Brokers.Security;
-
-public interface ISecurityBroker
-{
-    string HashPassword(string password);
-
-    bool VerifyPassWord(string password, string hash);
-}
